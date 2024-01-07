f52f4950a4de3cd2c3500e103fd8d3d42abd89ae
Dmitr <dmitr.yudin@mail.ru>
2024-01-07 10:30:07 +0300

    1

--- valentino/lib/buisiness/menu_bloc/menu_bloc.dart
+++ valentino/lib/buisiness/menu_bloc/menu_bloc.dart
@@ -8,15 +8,23 @@ part 'menu_state.dart';
 
 class MenuBloc extends Bloc<MenuEvent, MenuState> {
   MenuBloc() : super(MenuState(menuStatus: MenuStatus.initial)) {
-    on<MenuEvent>((event, emit)async {
+    on<MenuEvent>((event, emit) async {
+      MenuHttpModel menuHttpModel;
+      MenuState menuState;
+      try {
+        menuHttpModel = await MenuRepository().getCachedMenu();
+        menuState = MenuState(menuStatus: MenuStatus.done);
+        menuState.menuHttpModel = menuHttpModel;
 
-      MenuHttpModel menuHttpModel = await MenuRepository().getMenu();
-      MenuState menuState = MenuState(menuStatus: MenuStatus.done);
+        //  print (menuHttpModel);
+        emit(menuState);
+      } catch (_) {}
+      menuHttpModel = await MenuRepository().syncMenu();
+      menuState = MenuState(menuStatus: MenuStatus.done);
       menuState.menuHttpModel = menuHttpModel;
-     print(menuHttpModel.menu!.length);
-    //  print (menuHttpModel);
-     emit(menuState);
-     // emit();
+      //  print (menuHttpModel);
+      emit(menuState);
+      // emit();
       // TODO: implement event handler
     });
   }
