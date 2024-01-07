f821af4f2edd9c2b0f28c47ad58496cff0016451
Dmitr <dmitr.yudin@mail.ru>
2024-01-07 10:22:00 +0300

    val

--- data_layer/lib/network/menu_repository.dart
+++ data_layer/lib/network/menu_repository.dart
@@ -5,9 +5,20 @@ import 'package:data_layer/network/mock.dart';
 import 'package:dio/dio.dart';
 
 class MenuRepository {
-  Future<MenuHttpModel> getMenu() async {
-    Response responce = await Dio().get('http://91.222.236.176:8880/api/menu/');
-    //  print(responce.data);
+  Future<MenuHttpModel> syncMenu() async {
+    await Hive.initFlutter();
+    Response responce = await Dio().get('http://91.222.236.176:8880/api/menu');
+    var box = await Hive.openBox('menuBox1');
+
+    await box.put('menu', json.encode(responce.data));
     return MenuHttpModel.fromJson(responce.data);
+  }
+
+  Future<MenuHttpModel> getCachedMenu() async {
+    await Hive.initFlutter();
+    var box = await Hive.openBox('menuBox1');
+    Map<String, dynamic> temp = {};
+    var data = json.decode(box.get('menu'));
+    return MenuHttpModel.fromJson(data);
   }
 }
